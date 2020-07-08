011569ebb21e921921f426669dc1e535d5721d39
Jake Froehlich <jake.j.froehlich@gmail.com>
2020-07-08 18:57:24 -0400

    i feel like I'm doing it right but I'm not...

--- actions.js
+++ actions.js
@@ -4,4 +4,29 @@ const axios = require('axios');
 const { GOT_DESSERTS_DATA, NEW_DESSERT_DATA } = require('./types');
 
 /* YOUR CODE GOES HERE */
+
+function gotDesserts()  {
+    return {
+        type : GOT_DESSERTS_DATA
+    }
+}
+
+const addDessert = (dessert) => {
+    return {
+        type : NEW_DESSERT_DATA,
+        dessert,
+    }
+}
+
+
+const fetchDesserts = () => {
+    return (dispatch) => {
+        axios.get('/api/desserts')
+            .then((data) => dispatch(gotDesserts, data))
+    }
+}
+
+const postDessert = () => {
+
+}
 module.exports = { gotDesserts, addDessert, fetchDesserts, postDessert };
